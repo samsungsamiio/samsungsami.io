f2b214786b94772756ecbade66a04d0235edd671
M. Auayan <sxtxixtxcxh@gmail.com>
2015-02-10 11:38:50 -0800

    add subdomain to site_domain

--- config.rb
+++ config.rb
@@ -65,7 +65,7 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-set :site_domain, 'http://samsungsami.io/'
+set :site_domain, 'http://developer.samsungsami.io/'
 
 # Build-specific configuration
 configure :build do
